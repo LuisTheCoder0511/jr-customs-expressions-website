62b6c057ea1702d97d9f8e2f27b0b06e09494acb
OfficialLR <luroma0511@icloud.com>
2025-04-06 14:03:42 -0400

    Add Backblaze authentication

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 from api.database import oracle
+from api.bucket.backblaze import backblaze
 from api.blueprints.home import blueprint as home_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(home_blueprint)
 
 if __name__ == "__main__":
+    backblaze.__authenticate__()
     oracle.__run__()
     PORT = 8001
     app.run(host='0.0.0.0', port=PORT, debug=True, use_reloader=False)
